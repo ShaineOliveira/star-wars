6b7691f502263ccb262e692568b3471e607037c8
ShaineOliveira <shaineoliveira77@hotmail.com>
2024-02-21 17:54:01 -0300

    Adicionando MapStruct.

--- src/main/java/br/com/shaine/starwarsapi/mapper/RebeldeMapper.java
+++ src/main/java/br/com/shaine/starwarsapi/mapper/RebeldeMapper.java
@@ -3,6 +3,7 @@ package br.com.shaine.starwarsapi.mapper;
 import br.com.shaine.starwarsapi.dto.RebeldeRequestDto;
 import br.com.shaine.starwarsapi.model.Rebelde;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
